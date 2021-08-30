9453b50fd3af15e7c6e74463954a5ae453568be5
Jenny Lin <misslinyang91@gmail.com>
2021-08-31 09:13:25 +1000

    made the gallery clickable

---
+++ app.js
@@ -0,0 +1,27 @@
+// const currentImg = document.querySelector(".current-img");
+// const imgs = document.querySelectorAll(".all-imgs img");
+
+// imgs.forEach(function (img) {
+//   img.addEventListener("click", function (e) {
+//     currentImg.src = e.target.src;
+
+//     currentImg.classList.add("animate-imgs");
+//     setTimeout(() => {
+//       currentImg.classList.remove("animate-imgs");
+//     }, 400);
+//   });
+// });
+
+const currentImg = document.querySelector(".current-img");
+const imgs = document.querySelectorAll(".all-imgs img");
+
+imgs.forEach(function (img) {
+  img.addEventListener("click", function (e) {
+    currentImg.src = e.target.src;
+
+    currentImg.classList.add("animate-imgs");
+    setTimeout(function () {
+      currentImg.classList.remove("animate-imgs");
+    }, 400);
+  });
+});
